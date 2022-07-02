7041d20fe9cb13fbbfe77bb3bede8df9c19ef18b
rassihin <ilya_rassihin@inbox.ru>
2022-07-03 02:46:25 +0300

    second commit

--- app/src/main/java/com/rassihin/weatherapp/MainActivity.kt
+++ app/src/main/java/com/rassihin/weatherapp/MainActivity.kt
@@ -2,12 +2,31 @@ package com.rassihin.weatherapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
+
+    private var user_field: EditText? = null
+    private var mainButton: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        user_field = findViewById(R.id.editTextMain)
+        mainButton = findViewById(R.id.buttonMain)
+
+        mainButton?.setOnClickListener{
+            if(user_field?.text?.toString()?.trim()?.equals("")!!)
+                Toast.makeText(this, "Enter your city", Toast.LENGTH_LONG).show()
+            else {
+                var city: String = user_field?.text.toString()
+                var key: String = ""
+            }
+        }
     }
 }
